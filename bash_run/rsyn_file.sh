4e6f89d31dd67ba1ee0c4e3c937c77d18ee35a2a
tanlm <tanlm@datascience.com.vn>
2020-06-03 11:38:16 +0700

    add rsyn file and tensorboard view

--- bash_run/rsyn_file.sh
+++ bash_run/rsyn_file.sh
@@ -1,14 +1,21 @@
-#!/bin/bash
 set -e
-REMOTE_IP="38499"
-REPORT_PORT=""
-REMOTE_DIR=""
-LOCAL_DIR=""
 
+REMOTE_IP="34.87.77.5"
+REMOTE_PORT="34375"
+REMOTE_TENSORBOARD_PORT="6006"
+LOCAL_PORT="6009"
+REMOTE_DIR="/root/data/save_dir"
+LOCAL_DIR="/home/tanlm/Downloads/lgg-mri-segmentation/remote_save_dir"
+TIME_UPDATE=10 #second
+
+#--------------------------------------
+echo "ssh -N -f -L ${LOCAL_PORT}:127.0.0.1:${REMOTE_TENSORBOARD_PORT} -p ${REMOTE_PORT} root@${REMOTE_IP}"
+sh -c "ssh -N -f -L ${LOCAL_PORT}:127.0.0.1:${REMOTE_TENSORBOARD_PORT} -p ${REMOTE_PORT} root@${REMOTE_IP}" &
+echo "rsync -avzhe ssh -p ${REMOTE_PORT} root@${REMOTE_IP}:${REMOTE_DIR} ${LOCAL_DIR}"
 while [ true ]
 do
   echo "Rsyn from remote server"
-  rsync -avzhe "ssh -p 38499 root@34.87.77.5:/root/Project/pytorch-deeplab-xception/run /data2/tanlm/Project/pytorch-deeplab-xception/update"
+  sh -c "rsync -avzhe \"ssh -p ${REMOTE_PORT}\" root@${REMOTE_IP}:${REMOTE_DIR} ${LOCAL_DIR}"
   echo "Sleeping"
-  sleep 10
+  sleep $TIME_UPDATE
 done
